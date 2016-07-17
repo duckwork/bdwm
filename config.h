873e6b9eea0c1a08504f692ee1b00b71f3b91914
Case Duckworth <cased123@gmail.com>
2016-07-17 15:09:29 -0500

    Change default colors and modkey

--- config.h
+++ config.h
@@ -1,14 +1,14 @@
-#define MOD		SUPER		/* modifier for mouse resize and move */
+#define MOD		ALT /* modifier for mouse resize and move */
 
 /* INNER + OUTER should not be greater than BORDERWIDTH */
 #define BORDERWIDTH	4		/*  full  border width */
-#define INNER		3		/*  inner border width */
-#define OUTER		1		/*  outer border width */
+#define INNER		1		/*  inner border width */
+#define OUTER		3		/*  outer border width */
 
 /* colors */
-#define FOCUSCOL	0x18191A
-#define UNFOCUSCOL	0x111213
-#define OUTERCOL	0x000000
+#define FOCUSCOL	0xFFFF00
+#define UNFOCUSCOL	0x222222
+#define OUTERCOL	0x111111
 
 /* resize and move by mouse? */
 #define ENABLE_MOUSE
